eef18452bb214b7020117b919b0cca1f4fe6dd95
PENGUINLIONG <admin@penguinliong.moe>
2017-12-30 16:31:40 +0800

    Fixed timing

--- src/service.rs
+++ src/service.rs
@@ -19,12 +19,21 @@ impl Service for WritiumService {
     type Future = Box<::futures::future::Future<Item=Self::Response, Error=Self::Error>>;
     fn call(&self, req: HyperRequest) -> Self::Future {
         use std::time::Instant;
+        use futures::Future;
+
+        let method = req.method().clone();
         let from = Instant::now();
         // When the write lock is not released, the option can never be `None`.
         let future = self.0.read().unwrap().as_ref().unwrap().route(req);
         let delta = from.elapsed();
         let delta = (delta.as_secs() as f64) * 1000.0 + (delta.subsec_nanos() as f64) / 1_000_000.0;
-        info!("Request processed. (time = {}ms)", delta);
-        future
+        let future = future.then(move |result| {
+            match result {
+                Ok(ref res) => info!("{} -> {} (time = {}ms)", method.as_ref(), res.status(), delta),
+                Err(ref err) => warn!("Hyper error occured: {}", err),
+            }
+            result
+        });
+        Box::new(future)
     }
 }
